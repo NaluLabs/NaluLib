e3344a9cb1767130c3e10811f6f9c88bd4a16016
fabioCollini <fabio.collini@gmail.com>
2017-05-11 12:50:57 +0200

    Fixed logging

--- lib/src/main/java/com/nalulabs/lib/logs/LogUtils.java
+++ lib/src/main/java/com/nalulabs/lib/logs/LogUtils.java
@@ -29,28 +29,17 @@ public class LogUtils
         RxJavaPlugins.setErrorHandler(e -> {
             if (e instanceof UndeliverableException) {
                 e = e.getCause();
+                if (e instanceof IOException) {
+                    // fine, irrelevant network problem or API that throws on cancellation
+                    return;
+                }
+                if (e instanceof InterruptedException) {
+                    // fine, some blocking code was interrupted by a dispose call
+                    return;
+                }
             }
-            if (e instanceof IOException) {
-                // fine, irrelevant network problem or API that throws on cancellation
-                return;
-            }
-            if (e instanceof InterruptedException) {
-                // fine, some blocking code was interrupted by a dispose call
-                return;
-            }
-            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
-                // that's likely a bug in the application
-                Thread.currentThread().getUncaughtExceptionHandler()
-                        .uncaughtException(Thread.currentThread(), e);
-                return;
-            }
-            if (e instanceof IllegalStateException) {
-                // that's a bug in RxJava or in a custom operator
-                Thread.currentThread().getUncaughtExceptionHandler()
-                        .uncaughtException(Thread.currentThread(), e);
-                return;
-            }
-            Timber.w("Undeliverable exception received, not sure what to do", e);
+            Thread.currentThread().getUncaughtExceptionHandler()
+                    .uncaughtException(Thread.currentThread(), e);
         });
     }
 }
